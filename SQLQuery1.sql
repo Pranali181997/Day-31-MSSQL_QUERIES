06d763f8c539f77c21ebf8f205dbe1c0f5fa0cef
Pranali <pranalilambat00@gmail.com>
2022-03-24 13:45:51 +0530

    [Pranali]Add:CreateTable

--- SQLQuery1.sql
+++
@@ -1,7 +0,0 @@
---UC-1-CREATE DATA BASE
-Create database Payroll_Service
-
---UC-2-
-Create table employee_Payroll(Emp_ID int not null,Emp_NAME varchar(20),SALARY int not null,STARTDATE date)
-
-select * from employee_Payroll;
---
+++ SQLQuery2.sql
@@ -0,0 +1,5 @@
+--UC-1-CREATE DATA BASE
+Create database Emp_Payroll_Service
+
+--UC-2-CREATETABLE
+Create table emp_Payroll(Emp_ID int identity(100,110),Emp_NAME varchar(20),SALARY int,STARTDATE date)
