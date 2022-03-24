fc7f595814a19faa8a9020036d41f5065cc6cfd4
Pranali <pranalilambat00@gmail.com>
2022-03-24 13:48:57 +0530

    [Pranali]Add:CRUDOperation

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -3,3 +3,12 @@ Create database Emp_Payroll_Service
 
 --UC-2-CREATETABLE
 Create table emp_Payroll(Emp_ID int identity(100,110),Emp_NAME varchar(20),SALARY int,STARTDATE date)
+
+ --UC-3-CRUD OPERATION
+Insert into emp_Payroll(Emp_NAME,SALARY,STARTDATE) 
+values
+ ('Bill', 150000, '2018-01-03'),
+ ('Terisa', 200000, '2019-11-13'),
+ ('Charlie', 500000, '2020-05-21')
+
+ 
